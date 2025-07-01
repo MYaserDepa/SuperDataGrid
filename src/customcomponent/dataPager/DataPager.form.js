8976d771fa5736e86d97f2b799a32f522f978ac2
MYaserDepa <mohamed.yaser@depa.com>
2025-07-01 16:17:48 +0400

    Removed unnecessary code

--- src/customcomponent/dataPager/DataPager.form.js
+++ src/customcomponent/dataPager/DataPager.form.js
@@ -2,15 +2,12 @@ import { Formio } from "@formio/js";
 
 const baseEditForm = Formio.Components.baseEditForm;
 import DataPagerEditDisplay from "./editForm/DataPager.edit.display.js";
+
 export default function (...extend) {
     return baseEditForm([
         {
             key: 'display',
             components: DataPagerEditDisplay
-        },
-        {
-            key: 'layout',
-            ignore: true
         }
     ], ...extend)
 }
--- src/customcomponent/dataPager/editForm/DataPager.edit.display.js
+++ src/customcomponent/dataPager/editForm/DataPager.edit.display.js
@@ -1,47 +1,9 @@
 export default [
     {
         type: 'number',
-        key: 'numberOfIcons',
-        label: 'Number of Icons',
-        input: 'true',
-        tooltip: "The number of icons displayed in the form"
-    },
-    {
-        type: 'textfield',
-        key: 'icon',
-        label: 'Icon',
-        input: 'true',
-        tooltip: 'The bootstrap icon class that will go in the <i> tag'
-    },
-    {
-        type: 'textfield',
-        key: 'color',
-        label: 'Color',
-        input: 'true',
-        tooltip: 'The color of the icons'
-    },
-    {
-        type: 'textfield',
-        key: 'iconSize',
-        label: 'Icon Size',
-        tooltip: 'The size of the icon'
-    },
-    {
-        key: 'placeholder',
-        ignore: true
-    },
-    {
-        type: 'number',
         key: 'pageSize',
         label: 'Default Page Size',
         input: true,
         tooltip: 'The default number of items per page.'
-    },
-    {
-        type: 'tags',
-        key: 'pageSizeOptions',
-        label: 'Page Size Options',
-        input: true,
-        tooltip: 'Available options for items per page.'
     }
 ]
