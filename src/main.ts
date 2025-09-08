9425c90c44cab22b70006f4747974c6daa29df37
MYaserDepa <mohamed.yaser@depa.com>
2025-09-08 11:10:52 +0400

    Formatting

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,21 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Formio } from '@formio/angular';
 import dataPager from './customcomponent/dataPager/DataPager';
-import {dataPagerTemplate} from './customcomponent/templates/form';
+import { dataPagerTemplate } from './customcomponent/templates/form';
 
 Formio.use({
-  components: {
-    dataPager,
-  },
-  templates: {
-    bootstrap: {
-      dataPager: {
-        form: dataPagerTemplate,
-      },
-    },
-  },
+	components: {
+		dataPager,
+	},
+	templates: {
+		bootstrap: {
+			dataPager: {
+				form: dataPagerTemplate,
+			},
+		},
+	},
 });
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
+	console.error(err)
 );
