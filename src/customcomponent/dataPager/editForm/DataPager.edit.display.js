fc510decff0fea04740316976c7a8a9f50dd77ac
MYaserDepa <mohamed.yaser@depa.com>
2025-09-08 10:40:09 +0400

    Default pageLimit value = 5

--- src/customcomponent/dataPager/editForm/DataPager.edit.display.js
+++ src/customcomponent/dataPager/editForm/DataPager.edit.display.js
@@ -4,15 +4,10 @@ export default [
 		key: "pageLimit",
 		label: "Maximum Page Size",
 		input: true,
-		defaultValue: 4,
+		defaultValue: 5,
 		decimalLimit: 0, // no decimals
-		validateOn: "change", // validate immediately
 		validate: {
-			required: true,
 			min: 1,
-			step: 1,
-			custom:
-				"valid = (input !== undefined && Number(input) >= 1) ? true : 'Must be ≥ 1';",
 		},
 		tooltip: "The maximum number of items per page.",
 	},
