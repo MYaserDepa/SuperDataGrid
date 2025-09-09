8b186c8475a72cf9148cabb55feb681fe8ec4bbb
MYaserDepa <mohamed.yaser@depa.com>
2025-09-09 10:07:42 +0400

    Write the skeleton of SortAndFilter.js

--- src/customcomponent/sortAndFilter/SortAndFilter.js
+++ src/customcomponent/sortAndFilter/SortAndFilter.js
@@ -12,6 +12,7 @@ export default class SortAndFilter extends Component {
 				type: "sortAndFilter",
 				label: "Sort And Filter",
 				key: "sortAndFilter",
+				gridToAttach: "",
 			},
 			...extend
 		);
@@ -29,9 +30,50 @@ export default class SortAndFilter extends Component {
 
 	constructor(component, options, data) {
 		super(component, options, data);
+		this.allGridRows = [];
+		this.targetComponent = null;
 	}
 
 	render() {
 		return super.render(this.renderTemplate("sortAndFilter", {}));
+	}
+
+	/**
+	 * Find the target component by key (searches recursively from root)
+	 */
+	findTargetComponent() {
+		if (!this.root || !this.component?.gridToAttach) return null;
+		try {
+			return this.root.getComponent(this.component.gridToAttach);
+		} catch (e) {
+			return null;
+		}
+	}
+
+	/**
+	 * Called after render; wires refs, finds target component, and initializes UI.
+	 */
+	attach(element) {
+		// Let base class attach first
+		super.attach(element);
+
+		// Load refs from template
+		this.loadRefs(element, {
+			searchInput: "single",
+		});
+
+		// Locate the data grid to attach to
+		this.targetComponent = this.findTargetComponent();
+
+		if (!this.targetComponent) {
+			return;
+		}
+	}
+
+	/**
+	 * Cleanup listeners when component is removed
+	 */
+	detach() {
+		return super.detach();
 	}
 }
