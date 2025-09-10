1aa3cdbb283d9a0dc49b9883b4a4ee4378c2b76e
MYaserDepa <mohamed.yaser@depa.com>
2025-09-10 14:38:05 +0400

    Formatting

--- src/customcomponent/templates/form.js
+++ src/customcomponent/templates/form.js
@@ -37,12 +37,11 @@ export function sortAndFilterTemplate(ctx) {
 	return `
   <div ref="sortAndFilter" class="sortAndFilter-${compKey}" style="border: 3px solid #ddd; padding: 10px; overflow-x: auto;">
     <div class="sortAndFilter-controls" style="display: flex; flex-wrap: nowrap; gap: 20px; align-items: flex-start; min-width: 520px;">
-      
       <div class="filter-section" style="flex: 1;">
         <label style="display:block; font-weight: 600; margin-bottom: 5px;">Filter</label>
         <input type="text" placeholder="Search in any column..." ref="searchInput" style="padding: 5px; width: 100%; border: 1px solid #ced4da; border-radius: .25rem;" />
       </div>
-      
+
       <div class="sort-section" style="flex: 1;">
         <label style="display:block; font-weight: 600; margin-bottom: 5px;">Sort</label>
         <div style="display: flex; align-items: center;">
